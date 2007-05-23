226367dc83850922903552d3ae86dbed6797be98
Darrell Schiebel <darrell@schiebel.us>
2007-05-23 20:28:12 +0000

    took out the debug line...

--- ccmtools/src/ccmtools/CppGenerator/CppLocalGeneratorImpl.java
+++ ccmtools/src/ccmtools/CppGenerator/CppLocalGeneratorImpl.java
@@ -557,7 +557,6 @@ public class CppLocalGeneratorImpl extends CppGenerator
             }
 
             Template t = template_manager.getTemplate((idl_op_type instanceof MInterfaceDef ? "WrapperForwardCmptResult" : "WrapperForward"), current_name);
-	    System.err.println(">>>>>>>>>> " + vars + " - " + t + " - " + wvars + "\n");
             vars.put("WrapperForward", t.substituteVariables(wvars));
         		
             if (idl_op_type instanceof MInterfaceDef) {
